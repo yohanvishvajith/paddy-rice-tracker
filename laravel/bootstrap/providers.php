cc309aee525edb2fd21deb896c119972d5c7abbb
yohan vishvajith <yohanvishvajith@gmail.com>
2025-10-31 06:53:54 -0600

    fg

--- laravel/bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\MillerPanelProvider::class,
-    App\Providers\Filament\CollecterPanelProvider::class,
-    App\Providers\Filament\WholesalerPanelProvider::class,
-];
